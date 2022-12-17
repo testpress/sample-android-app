f88205b1347643deac77f7dd29d1a74edbfd98a4
Karthik <karthik@testpress.in>
2022-12-17 15:13:26 +0530

    fix: Add sample implementation for playbackstate listener

--- app/src/main/java/com/example/streams/PlayerActivity.kt
+++ app/src/main/java/com/example/streams/PlayerActivity.kt
@@ -2,16 +2,16 @@ package com.example.streams
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.tpstream.player.InitializationListener
-import com.tpstream.player.TpInitParams
-import com.tpstream.player.TpStreamPlayer
-import com.tpstream.player.TpStreamPlayerFragment
+import android.util.Log
+import androidx.media3.common.*
+import com.tpstream.player.*
 
 class PlayerActivity : AppCompatActivity() {
     lateinit var playerFragment: TpStreamPlayerFragment;
     private val accessToken = "bbf23112-0c14-4519-a848-73c95cb024ac"
     private val videoId = "E44ulfSWhYx"
     private val orgCode = "drm"
+    val TAG = "PlayerActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,5 +30,74 @@ class PlayerActivity : AppCompatActivity() {
             }
         });
         playerFragment.enableAutoFullScreenOnRotate()
+        playerFragment.playbackStateListener = object : TPPlayerListener {
+            override fun onDeviceInfoChanged(deviceInfo: DeviceInfo) {
+                Log.d(TAG, "onDeviceInfoChanged: ")
+            }
+
+            override fun onEvents(player: TpStreamPlayer?, events: Player.Events) {
+                Log.d(TAG, "onEvents: ")
+            }
+
+            override fun onIsLoadingChanged(loading: Boolean) {
+                Log.d(TAG, "onIsLoadingChanged: ")
+            }
+
+            override fun onIsPlayingChanged(playing: Boolean) {
+                Log.d(TAG, "onIsPlayingChanged: ")
+            }
+
+            override fun onMetadata(metadata: Metadata) {
+                Log.d(TAG, "onMetadata: ")
+            }
+
+            override fun onPlayWhenReadyChanged(playWhenReady: Boolean, reason: Int) {
+                Log.d(TAG, "onPlayWhenReadyChanged: ")
+            }
+
+            override fun onPlaybackStateChanged(playbackState: Int) {
+                /*
+                * Playback states can be compared with TpStreamPlayer.PLAYBACK_STATE
+                * */
+                Log.d(TAG, "onPlaybackStateChanged: ")
+            }
+
+            override fun onPlayerError(error: PlaybackException) {
+                Log.d(TAG, "onPlayerError: ")
+            }
+
+            override fun onPlayerErrorChanged(error: PlaybackException?) {
+                Log.d(TAG, "onPlayerErrorChanged: ")
+            }
+
+            override fun onPositionDiscontinuity(
+                oldPosition: Player.PositionInfo,
+                newPosition: Player.PositionInfo,
+                reason: Int
+            ) {
+                Log.d(TAG, "onPositionDiscontinuity: ")
+            }
+
+            override fun onSeekBackIncrementChanged(seekBackIncrementMs: Long) {
+                Log.d(TAG, "onSeekBackIncrementChanged: ")
+            }
+
+            override fun onSeekForwardIncrementChanged(seekForwardIncrementMs: Long) {
+                Log.d(TAG, "onSeekForwardIncrementChanged: ")
+            }
+
+            override fun onTimelineChanged(timeline: Timeline, reason: Int) {
+                Log.d(TAG, "onTimelineChanged: ")
+            }
+
+            override fun onTracksChanged(tracks: Tracks) {
+                Log.d(TAG, "onTracksChanged: ")
+            }
+
+            override fun onVideoSizeChanged(videoSize: VideoSize) {
+                Log.d(TAG, "onVideoSizeChanged: ")
+            }
+
+        }
     }
 }
